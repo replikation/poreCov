0138605f64552325450750a7de365d93078f1760
dataspott <Ricc.Spott@gmx.de>
2021-06-04 15:55:17 +0200

    cleaned search-pattern

--- bin/split_fasta.py
+++ bin/split_fasta.py
@@ -41,7 +41,7 @@ for fasta_file in sys.argv[1:]:
                 if seq_name in sequence_names:
                     log(f'WARNING: Duplicate sequence name: {seq_name}')
                     # add number to seq_name, according to how often seq_name alrready appeared
-                    duplicate_seq_name_pattern = f'{seq_name}_duplicate**'
+                    duplicate_seq_name_pattern = f'{seq_name}_duplicate*'
                     seq_name += f'_duplicate{int(len([item for item in sequence_names if item == seq_name or fnmatch.fnmatch(item ,duplicate_seq_name_pattern)])):02d}'
                 
                 # save
