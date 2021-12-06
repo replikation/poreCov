b1092879705997aca670d6e1b03b746e439e4308
Sebastian Krautwurst <raverjay@gmail.com>
2021-12-06 18:11:02 +0100

    * as stop

--- bin/summary_report.py
+++ bin/summary_report.py
@@ -87,8 +87,8 @@ def nt2aa(nucleotides, allow_uracil=True, allow_stop_codon=False):
             'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
             'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S',
             'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L',
-            'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_',
-            'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W'
+            'TAC':'Y', 'TAT':'Y', 'TAA':'*', 'TAG':'*',
+            'TGC':'C', 'TGT':'C', 'TGA':'*', 'TGG':'W'
         }
 
     if allow_uracil:
@@ -100,7 +100,7 @@ def nt2aa(nucleotides, allow_uracil=True, allow_stop_codon=False):
         peptide += triplets[codon]
 
     # stop codon
-    if '_' in peptide:
+    if '*' in peptide:
         return None
     
     return peptide
