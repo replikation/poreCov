b46324b3b2280519500baaf16f3c61469aeed61c
Sebastian Krautwurst <raverjay@gmail.com>
2022-04-05 14:30:08 +0200

    fixed version warning msg

--- bin/summary_report.py
+++ bin/summary_report.py
@@ -117,8 +117,8 @@ class SummaryReport():
         pl_param = f'<a href="https://cov-lineages.org/resources/pangolin/requirements.html"><b>pangolin-data</b></a> version'
         pl_val = f'{self.pangolindata_version}'
 
-        year, month, day = self.pangolindata_version.split('-')
-        if int(year) <= 2021 and int(month) <= 10:
+        v1, v2, v3 = self.pangolindata_version.split('-')
+        if int(v1) <= 1 and int(v2) <= 2 and int(v3) <= 132
             pl_val += warning_msg
 
         self.add_param(pl_param, pl_val)
