447c0a10a5c22668311251377a857715b00b854e
Sebastian Krautwurst <raverjay@gmail.com>
2022-04-05 14:35:56 +0200

    fix pdata version

--- bin/summary_report.py
+++ bin/summary_report.py
@@ -117,7 +117,7 @@ class SummaryReport():
         pl_param = f'<a href="https://cov-lineages.org/resources/pangolin/requirements.html"><b>pangolin-data</b></a> version'
         pl_val = f'{self.pangolindata_version}'
 
-        v1, v2, v3 = self.pangolindata_version.split('-')
+        v1, v2, v3 = self.pangolindata_version.split('.')
         if int(v1) <= 1 and int(v2) <= 2 and int(v3) <= 132:
             pl_val += warning_msg
 
