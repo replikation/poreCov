31404e3a3f5f98dac9701b5b8bf2ebd87204350e
DataSpott <ricc.spott@gmx.de>
2022-10-14 15:03:54 +0200

    Changed behaviour of split-fasta.py from splitting fasta-headers at the first whitespace and taking only the first part to replacing whitespaces with "_". Should avoid problems with fastas containing whitespaces in the header (like GISAID-fastas from Hong Kong & Sri Lanka) being labelled as duplicates of the same sequence.

--- bin/split_fasta.py
+++ bin/split_fasta.py
@@ -36,11 +36,11 @@ for fasta_file in sys.argv[1:]:
         if line.startswith('>'):
 
             # new sequence
-            seq_name = line.strip().split()[0][1:]
+            seq_name = line.strip()[1:]
             assert seq_name != '', f'Empty header in file: {fasta_file}'
             
             # sanitize
-            seq_name = seq_name.replace('/', '_').replace(':', '_').replace('|','_')
+            seq_name = seq_name.replace(' ', '_').replace('/', '_').replace(':', '_').replace('|','_')
 
             # handle duplicates
             if seq_name in sequence_names:
